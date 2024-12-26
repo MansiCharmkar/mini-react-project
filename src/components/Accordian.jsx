2464599443d3fad2021714a8b1b0e68fbc713682
Mansi Charmkar <mansic632@gmail.com>
2024-12-26 18:16:19 +0530

    Acordion Project Copmleted

--- src/components/Accordian.jsx
+++ src/components/Accordian.jsx
@@ -1,15 +1,30 @@
 import { useState } from "react";
-import style from "../style/Accordian.module.css";
+import styles from "../style/Accordian.module.css";
+import { HiMiniMinus, HiMiniPlus } from "react-icons/hi2";
 function Accordian(props) {
     const { content, color } = props;
-    const [open, setOpen] = useState(false);
+    const [open, setOpen] = useState(null);
+
+    // open == null > all closed
+
+    // curr index
+    // 1 1 => null
+    // 1 2 => 2
+
+    function handleClick(index) {
+        setOpen((currState) => (currState === index ? null : index));
+    }
+
     return (
-        <div>
+        <div className={styles.container}>
             {content.map(function (content, index) {
                 return (
                     <Row
                         title={content.title}
                         details={content.details}
+                        handleClick={handleClick}
+                        index={index}
+                        isOpen={open === index}
                         key={index}
                     />
                 );
@@ -18,11 +33,15 @@ function Accordian(props) {
     );
 }
 function Row(props) {
-    const { title, details } = props;
+    const { title, details, handleClick, index, isOpen } = props;
     return (
-        <div>
-            <h3>{title}</h3>
-            <p>{details}</p>
+        <div onClick={() => handleClick(index)} className={styles.row}>
+            <h3 className={styles.title}>
+                <span>
+                    {title} {isOpen ? <HiMiniMinus /> : <HiMiniPlus />}
+                </span>
+            </h3>
+            {isOpen && <p className={styles.details}>{details}</p>}
         </div>
     );
 }
